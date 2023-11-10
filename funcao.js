47111f2751f048e09ac8fdb4edda7f9744fee173
luaann89 <127844960+luaann89@users.noreply.github.com>
2023-11-10 10:21:32 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,9 @@
+//Declare uma variavel "nome" com seu nome.
+//Crie uma função que imprima "olá, [seu nome]!" no console.
+var nome = "Luan";
+
+function saudacao() {
+    console.log("olá, " + nome + "!");
+}
+
+saudacao();
